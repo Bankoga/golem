76497a118225ade576b814056dea49ec6d7d6832
v <bankoga@gmail.com>
2018-07-23 12:25:25 -0500

    updated init with the props added plans for implementation stubbed get value, and strength methods removed unnecessary axon_ prefix

--- core/axon.py
+++ core/axon.py
@@ -1,17 +1,43 @@
 # contains the definitions for cell axons
+from usageLimits import *
 
 class Axon():
-    # properties
-    axon_value #-1,1
-    axon_refreshRate
-    axon_activationRate
-    axon_destinations = []
+    def __init__(self, activation_type, terminal_destinations, chemical_type):
+        """
+        an axon needs to know the activation type which determines if the axon outputs a positive, negative, or chemical value
+        'inhibitory' || 'excitory' || 'modulatory'
+        it also needs to know where the axon terminates
+        -1,1, or chemical is determined by cell type
+        """
+        self.value = self.get_value(activation_type, chemical_type)
+        """ refresh rate, and activation rate can be implemented via usage limits
+        self.refreshRate
+        self.activationRate
+        """
+        self.use_limits = UsageLimits()
+        """
+            destinations are determined by the cell location
+            should we pass destinations to the axon from the cell instead of passing the cell location to the axon?
+                That makes more sense I guess
+        """
+        self.destinations = terminal_destinations
 
-    def __init__(self):
-        # an axon needs to know the cell type which determines if the axon outputs a positive, negative, or chemical value
-        # it also needs to know where the axon terminates
+    def get_value(activation_type, chemical_type):
+        if activation_type == 'inhibitory':
+            return -1
+        elif activation_type == 'excitory':
+            return 1
+        elif activation_type == 'modulatory' AND chemical_type != NULL:
+            TODO: implement modulatory axon terminals
+            raise SystemError('modulatory chemical type axons are not currently implemented')
+        else:
+            raise ValueError('either the activation_type was incorrect, or no chemical_type was supplied')
 
-    def axon_strength():
+    def strength():
+        TODO: implement so that that axon only checks during init which type of strength method to return based on value
+        TODO: implement axons getting weaker as the capacity diminishes
+        return {self.destinations, self.value }
     
     def reset():
-        axon_activation_rate = 0
+        # self.use_limits.
+        activation_rate = 0
