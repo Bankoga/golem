6c0eb07c9c23e57d8c1360fd4925470a10fd5bb2
v <bankoga@gmail.com>
2018-07-25 13:02:08 -0500

    Expanded on layer and region plans. They correlate Added method stub for getting destinations for a layer Commented out currently unnecessary num_layers prop

--- core/layer.py
+++ core/layer.py
@@ -13,8 +13,17 @@ class Layer():
         determine the number of excitory and inhibitory cells
         initialize all the cells
         """
+        """
+        Local aspects of a layer and/or region that can differ
+        - plasticity rules (inhib, excitory, modulatory) (timing, decay/potentiate funcs, etc...)
+        - cell types
+        - allowed layer destinations (with blanks for cross-region, and cross-pd destination insertion. Example: cortical layer 5 and 6)
+        - resource constraints (can each cell type have diff constraints?)
+        - what else
+        """
+
     
-    def activate():
+    def activate(self):
         """
             perhaps a new name will better fit but this is fine for now. It matches the cell func name
             collect all the input axons based on the connectivity profiles from the axon server(?) into input batches for the cells
@@ -23,3 +32,13 @@ class Layer():
         """
 
 
+    def get_destinations(self):
+        """ since each region type has it's own layer types, where should destination mappings live?
+        I feel that it may be best for layer destination mappings to live outside the class in a json, yaml, or other such file
+        Why?
+            the cortical region alone has 6 distinct layers with different output connection patterns, cell types, plasticity rules, (what else?)
+            There are at least 3 types of regions that I know need to be created, and at least two problem domains with undetermined regions
+            Those other problem domains should at least have 1 type of region each, though they will likely have more than one
+            So at this point, we can safely expect 5 or more regions that we need predefined to get info from
+            Greater flexibility results from localizing cell types, plas rules, resource constraints, connect patterns, etc... to each layer or region
+        """
--- core/region.py
+++ core/region.py
@@ -8,16 +8,21 @@ class Region():
         possibilities are currently limited to
             - Cortex
             - Relay
+            - Cerebellum
         Currently, I plan to extend for a subcortical series of problem domains, though it may just be one domain with lots of regions
-        perhaps the layers dict should be defined by the problem domain?
-            But then where would the common region definitions exist?
+            As well as add a brain stem problem domain
+            The issue with single PD subcort is that a single layer then actually be multiple layers which breaks the currentregion paradigm
+            In a sense, a region adds "height" to a problem domain that is otherwise length and width onlys
+        perhaps the layers dict should be defined by the problem domain? No. Problem Domain types define regions. Regions define layers
+            But then where would the common region definitions exist? In a separate file or class
             Where would the region to region connection profiles exist in that case?
+                If each region type is unique to a problem domain type, then R-R connections can be defined within the region type defs
     """
-    layers_dict = {"Cortex":6, "Relay": 4}
+    # layers_dict = {"Cortex":6, "Relay": 4}
 
     def __init__(self, region_key, length, width):
         self.name = region_key
         self.length = length
         self.width = width
          # could also be called height but num_layers is more useful
-        self.num_layers = layers_dict[self.name]
+        # self.num_layers = layers_dict[self.name]
