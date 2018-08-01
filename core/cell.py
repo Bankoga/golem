11f1539ef20351b12dc5ca1171434b171042bf41
v <bankoga@gmail.com>
2018-08-01 15:30:37 -0500

    Modified activate - added timestep - changed layer_state to destination_state

--- core/cell.py
+++ core/cell.py
@@ -76,6 +76,7 @@ class Cell():
         PD location in graph
         """
         return []
+
     def maintenance():
         """
         reset axon fatigue
@@ -92,7 +93,7 @@ class Cell():
 
     def threshhold_plasticity():
 
-    def activate(self, mode, layer_state, input_batch):
+    def activate(self, mode, timestep, destination_state, input_batch):
         """
         TODO: determine if we need refactory/cooldown can't activate period
         if polarity >= ap_thresh: polarity -= (ap_thresh + depolarization_rate)
