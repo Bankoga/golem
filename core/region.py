2a55a34ef12831ccd3f5f55653b7c1badde9aa27
v <bankoga@gmail.com>
2018-08-01 15:52:07 -0500

    Region changes Added location property updated create_layers with ind, and config added activate code, and todo for further work

--- core/region.py
+++ core/region.py
@@ -7,10 +7,8 @@ from layer import *
 def str_to_class(str):
     return getattr(sys.modules[__name__], str)
 
-
 class Region:
      """
-
             In a sense, a region adds "height" to a problem domain that is otherwise length and width onlys
         perhaps the layers dict should be defined by the problem domain? No. Problem Domain types define regions. Regions define layers
             But then where would the common region definitions exist? In a separate file or class
@@ -18,12 +16,12 @@ class Region:
                 If each region type is unique to a problem domain type, then R-R connections can be defined within the region type defs
     """
 
-    def __init__(self, source, region_type, length, width):
+    def __init__(self, source, location, region_type, length, width):
         self.name = region_type
-        self.location = [source]
+        self.location = location.append(source)
         self.length = length
         self.width = width
-        TODO: raise an exception if the yaml doesn't exist
+        TODO: raise an exception and exit if the yaml does not exist
         config_fname = 'region_confs\\{0}.yaml'.format(region_type)
         self.config = load(open(config_fname))
         self.layers = self.create_layers()
@@ -33,20 +31,14 @@ class Region:
          # could also be called height but num_layers is more useful
         # self.num_layers = layers_dict[self.name]
         layers = []
-        for i in self.config['layers']:
-            # if I want to do something like name, morphs, dests, order = i.values() then I should be using an ordered dict
-            name = i['name']
-            # if the layers config object is an ordered list, then order is unnecessary
-            # order = config.['order']
-            [layers[len(layers):] = [Layer(config, self.name, self.location, self.length, self.width)]
-        """
-        foreach distinct (layer_name, layer_details) in self.config['layers']:
-            create a new Layer() using the layer config, and self.location
-            add the new layer to the layers object
-            self.layer_config = self.get_layer_confs()
-        """
+        ind = 0
+        for l in self.config['layers']:
+            # if the layers config object is an ordered list, then order is an unnecessary config item
+            layers.append(Layer(l, self.name, self.location, self.length, self.width, ind))
+            ind+=1
         return layers
     
+    TODO: expand activate to have current mode, time step, and the layer keyed input batched
     def activate(self):
         """
             perhaps a new name will better fit but this is fine for now. It matches the cell func name
@@ -55,3 +47,7 @@ class Region:
                 collect the activation result of each layer on the input batch into an array
             return the layer results to the axon server for the current timestep
         """
+        activations = []
+        for l in layers:
+            activations.extend(l.activate())
+        return activations
