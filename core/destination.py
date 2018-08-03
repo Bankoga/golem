5465e3bcbf033d2bf38e915410e73f6f53c21b93
v <bankoga@gmail.com>
2018-08-03 13:57:38 -0500

    Destination changes - Actually added warning about init variable mutation - Noted question about distribution data downstream accessibility - Stubbed flow for get distributions of cells by type - Stubbed flow for get cell data based on distributions

--- core/destination.py
+++ core/destination.py
@@ -2,22 +2,58 @@ class Destination:
 
     def __init__(self, config, source, location, coord):
         TODO: change how the destination init works based on cell size to avoid if clauses or other such checks during activation
+        TODO: Confirm that inits do not mutate variables passed to them by accident
         self.location = location.extend([source, coord])
         self.point_size = config['point_size'] # num cells at the destination
         self.morphs = config['cell_morphology']
         self.dests = config['destinations']
-        self.cells = create_cells()
+        self.cells = self.create_cells()
 
     def create_cells(self):
         """
         create a number of cells equal to point_size, according to the rules for distribution, types, and destinations from the config
         # each cell type takes up a % of the num cells (point_type)
+        cell_type_distribution contains tuples of current count, max count given point_size,
+            and cell type data as calcd via the provided % of num cell type, and cell type data
         """
-        cell_type_distribution = []
+        TODO: should distribution data be determined via a prop in the init so that we can look at it later?
+        cell_type_distribution = self.get_distributions()
         cells = []
         for i in range(0, self.point_size):
-            cells.append(Cell())
+            c, cell_type_distribution = self.get_cell(cell_type_distribution)
+            cells.append(c)
         return cells
+
+    def get_distributions(self):
+        """
+        ct_dist = []
+        max_count = 0
+        ind = 0
+        ind_of_max = 0
+        total_count = 0
+        for each cell type
+            calc floored ct_max_num via cell type prob * point_size
+            total_count += ct_max_num
+            append new tuple for current count, max count
+            if max_count < ct_max_num:
+                ind_of_max = ind
+            ind++
+        count_diff = total_count - self.point_size
+        increase ct_max_num of ct_dist[ind_of_max] by count_diff
+        return ct_dist
+        """
+
+    def get_cell_data(self, ct_dist):
+        """
+        grab copy of first cell type tuple
+        if current count >= num cell - 1:
+            remove first tuple from list
+        elif:
+            increment current count in the ct_dist
+        use cell type dist to create
+        c = Cell(DETAILS)
+        return new cell, and ct_dist
+        """
 
     TODO: add mode, time step, and input batches to the activation method
     def activate(self):
