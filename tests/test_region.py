e55eb30daf9d6e6c9b1b9dc764dcc19ced4d5463
v <bankoga@gmail.com>
2018-08-06 14:37:25 -0500

    Added mem usage calcs to test region

--- tests/test_region.py
+++ tests/test_region.py
@@ -9,3 +9,9 @@ relay = dump(load(open('region_confs\\relay.yaml')))
 
 if the config layers object is a list, it preserves order of the layers
 if it is a dict, it sorts them
+
+def calc_mem_use(num_cells, edges_per_cell, cell_size):
+    py_edge_size = 96
+    byts = (num_cells * cell_size) + (num_cells * edges_per_cell * py_edge_size)
+    print('{0} terabytes'.format(byts / Math.pow(10,12)))
+    return 'wtf have I done?'
