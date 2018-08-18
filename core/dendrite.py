ac6858060b5ff6479169b323028cfd1409bf34cd
v <bankoga@gmail.com>
2018-08-18 17:15:15 -0500

    Dendrite changes - moved input history tracking to dendrites - added pseudocode for summate - added props to init - added pseudocode for synapse creation

--- core/dendrite.py
+++ core/dendrite.py
@@ -14,15 +14,29 @@ class Dendrite():
     synapse_weight_limits = 0.2 to 100? # this needs work but so does everything.... waaa
     synapse_potentiation_weight_function
     synapse_decay_weight_function
+    TODO: Determine if track the weight that was activated, time since active, or something other flag for use when calculating weight changes
 
     #methods
-    def __init__(self):
+    def __init__(self, source_location):
         # each dendrite chain/tree/? should be able to handle its own initialization once given the necessary parameters from the higher levels
+        # each dendrite knows the path key for the destination it pulls data from
+        self.source = source_location
+        TODO: what if instead of storing batch history, we count num steps since last use per synapse?
+        # uses num synapses * 2 instead of num synapses * 20
+        # though doing so does not track multiple uses within the stdp_window
+        self.inputBatchHistory = "sigh" #fixed length array/matrix of length stdp_window or a stack of inputs
 
     def dendrite_sensitivity():
         dendrite_sensitivity_decay_function # perhaps this is just a method instead of a parameter
 
-    def dendrite_summation():
+    def summate(self, input):
+        total = 0
+        """
+        weights = multiply synapse array by the input array
+        total = sum of values in weights
+        do plasticity
+        """
+        return total
 
     def synapse_plasticity():
         """
@@ -36,8 +50,13 @@ class Dendrite():
     def synapse_creation():
         """ for each synapse that is currently of 0 weight, and hasn't been just pruned, roll for creating a new synapse at a base creation weight """
         TODO: determine what works as a good weight starting weight for new synapses
-        baseCreationWeight = 10
+        base_creation_weight = 10
         base_synapse_creation_probability = 0.3 +- 0.25
+        for synapse in synapses:
+            if synapse >= 0:
+                roll for new synapse
+                if roll > base_synapse_creation_probability:
+                    add weight to synapse
 
     def synapse_prune():
         """ for all synapses below some threshhold weight are reduced to zero, and skipped during the next round of synapse creation """
