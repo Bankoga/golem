3a81bcc935d414084976d6c1b32990397f45dd63
v <bankoga@gmail.com>
2018-12-24 09:22:31 -0600

    Added info helper stub with example data stub

---
+++ helpers/info.py
@@ -0,0 +1,30 @@
+TODO: build tools for collecting system info from active golems
+# active environment : base
+# active env location : C:\ProgramData\Anaconda3
+# shell level : 1
+# user config file : C:\Users\sturmy\.condarc
+# populated config files :
+# conda version : 4.5.11
+# conda-build version : 3.4.1
+# python version : 3.6.4.final.0
+# base environment : C:\ProgramData\Anaconda3  (read only)
+# channel URLs : https://repo.anaconda.com/pkgs/main/win-64
+#           https://repo.anaconda.com/pkgs/main/noarch
+#           https://repo.anaconda.com/pkgs/free/win-64
+#           https://repo.anaconda.com/pkgs/free/noarch
+#           https://repo.anaconda.com/pkgs/r/win-64
+#           https://repo.anaconda.com/pkgs/r/noarch
+#           https://repo.anaconda.com/pkgs/pro/win-64
+#           https://repo.anaconda.com/pkgs/pro/noarch
+#           https://repo.anaconda.com/pkgs/msys2/win-64
+#           https://repo.anaconda.com/pkgs/msys2/noarch
+# package cache : C:\ProgramData\Anaconda3\pkgs
+#           C:\Users\sturmy\AppData\Local\conda\conda\pkgs
+# envs directories : C:\Users\sturmy\AppData\Local\conda\conda\envs
+#           C:\ProgramData\Anaconda3\envs
+#           C:\Users\sturmy\.conda\envs
+# platform : win-64
+# user-agent : conda/4.5.11 requests/2.18.4 CPython/3.6.4 Windows/10 Windows/10.0.17134
+# administrator : False
+# netrc file : None
+# offline mode : False
