94c293d383f42421445197cc832c988bcff7ccee
v <bankoga@gmail.com>
2019-01-02 11:43:12 -0600

    Added semantic quibble to cell

--- cell/cell.py
+++ cell/cell.py
@@ -9,6 +9,7 @@ from location import *
 
 # should the cell accept a potential plasticity parameters object so that we can alter plasticity in other areas of the brain_network? Probably
 class Cell:
+    TODO: Consider renaming cell to node
     TODO: Optim: move the cell constants to another file so that each cell does not duplicate unnecessary data
     def __init__(self, activation_type, cell_morphology, key, ploc, use_limits):
         """
