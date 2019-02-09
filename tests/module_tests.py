ec4c07a3e635eafb5bd8e051711a055798338b9a
v <bankoga@gmail.com>
2019-02-09 11:37:25 -0600

    fixed module feature name added stub for module unit tests

---
+++ tests/module_tests.py
@@ -0,0 +1,11 @@
+import unittest
+from hypothesis import *
+from module.py import *
+from string import ascii_lowercase
+# from config_tests_data.py import *
+
+class ModuleTests(unittest.TestCase):
+    TODO: Move the GOLEM conda env from conda envs in AppData local to the package...
+
+    def test_module_function_builder(lyrRls, lyrDtls, inMlds, outMlds, gendShpDscrps, lnks):
+        return False
