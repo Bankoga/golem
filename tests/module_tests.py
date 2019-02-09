e5d24de49aeac6b102a68541eb0a6bb5e04887b5
v <bankoga@gmail.com>
2019-02-09 11:48:19 -0600

    Added component and validator separation of golem pieces Stubbed validator test functions

--- tests/module_tests.py
+++ tests/module_tests.py
@@ -7,5 +7,41 @@ from string import ascii_lowercase
 class ModuleTests(unittest.TestCase):
     TODO: Move the GOLEM conda env from conda envs in AppData local to the package...
 
-    def test_module_function_builder(lyrRls, lyrDtls, inMlds, outMlds, gendShpDscrps, lnks):
+    def test_compose_function(inputMelds,funcType,procStageGroupsDict,procStageShape,procGroupInputMelds,procGroupDetails,procGroupOutputMelds,procOutputMelds,shapeComposition,outputMelds,linkMelds,linksDefined):
+        return False
+    
+    def test_inputMeldsValidation():
+        return False
+    
+    def test_funcType():
+        return False
+    
+    def test_procStageGroupsDict():
+        return False
+    
+    def test_procStageShape():
+        return False
+    
+    def test_procGroupInputMelds():
+        return False
+    
+    def test_procGroupDetails():
+        return False
+    
+    def test_procGroupOutputMelds():
+        return False
+    
+    def test_procOutputMelds():
+        return False
+    
+    def test_shapeComposition():
+        return False
+    
+    def test_outputMelds():
+        return False
+    
+    def test_linkMelds():
+        return False
+    
+    def test_linksDefined():
         return False
---
+++ validators/module_validator.py
@@ -0,0 +1,16 @@
+class moduleValidator():
+    def __init__(self):
+        pass
+
+    def inputMeldsValidation(self):
+    def funcTypeValidation(self):
+    def procStageGroupsDictValidation(self):
+    def procStageShapeValidation(self):
+    def procGroupInputMeldsValidation(self):
+    def procGroupDetailsValidation(self):
+    def procGroupOutputMeldsValidation(self):
+    def procOutputMeldsValidation(self):
+    def shapeCompositionValidation(self):
+    def outputMeldsValidation(self):
+    def linkMeldsValidation(self):
+    def linksDefinedValidation(self):
