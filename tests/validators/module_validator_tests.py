9bd1032c9a73b1460b3e5668b9b9fd824bb6c131
v <bankoga@gmail.com>
2019-02-10 10:57:08 -0600

    renamded config directories to be more python oriented

--- tests/validators/module_validator_tests.py
+++ tests/validators/module_validator_tests.py
@@ -8,27 +8,51 @@ class ModuleValidatorTests(unittest.TestCase):
     def setUp(self):
         self.validator = ModuleValidator()
 
+
     def test_input_melds_validation(self):
         self.assertTrue(self.validator.input_melds_validation())
+
+    @
     def test_func_type_validation(self):
         self.assertTrue(self.validator.func_type_validation())
+        self.assertIn(func)
+    
+    
     def test_proc_stage_groups_dict_validation(self):
         self.assertTrue(self.validator.proc_stage_groups_dict_validation())
+    
+    
     def test_proc_stage_shape_validation(self):
         self.assertTrue(self.validator.proc_stage_shape_validation())
+    
+    
     def test_proc_group_input_melds_validation(self):
         self.assertTrue(self.validator.proc_group_input_melds_validation())
+    
+    
     def test_proc_group_details_validation(self):
         self.assertTrue(self.validator.proc_group_details_validation())
+    
+    
     def test_proc_group_output_melds_validation(self):
         self.assertTrue(self.validator.proc_group_output_melds_validation())
+    
+    
     def test_proc_output_melds_validation(self):
         self.assertTrue(self.validator.proc_output_melds_validation())
+    
+    
     def test_shape_composition_validation(self):
         self.assertTrue(self.validator.shape_composition_validation())
+    
+    
     def test_output_melds_validation(self):
         self.assertTrue(self.validator.output_melds_validation())
+    
+    
     def test_link_melds_validation(self):
         self.assertTrue(self.validator.link_melds_validation())
+    
+    
     def test_links_defined_validation(self):
         self.assertTrue(self.validator.links_defined_validation())
