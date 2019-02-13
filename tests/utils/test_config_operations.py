49934a8ca7018102b13ba82c7df7b94278637fec
v <bankoga@gmail.com>
2019-02-13 14:17:34 -0600

    Test implemented build module entry and build full config

--- tests/utils/test_config_operations.py
+++ tests/utils/test_config_operations.py
@@ -19,75 +19,74 @@ class TestConfigOperations(unittest.TestCase):
     self.config = read('Test','golem')
 
   def test_build_full_config(self):
-    full = build_full_config(self.config)
-    self.assertTrue(full['modules'][0]['proc_groups']['type_data']['id']=='PICG')
-    self.assertNotEqual(self.config,full)
+    build_full_config(self.config)
+    self.assertTrue('sensor' in self.config['modules'][0]['proc_groups'])
+    self.assertNotEqual(read('Test','golem'),self.config)
 
   # @given(st.integers(max_value=6))
   def test_build_module_entry(self):
-    module_num = 2
+    module_num = 6
     module = self.config['modules'][module_num]
     # building a module config means turning all of the properties into a single list of processing group properties ready for melding
     # thus building a module config is composed of several steps. In essence, we extend the module object using the proc object
     # first we must collect the missing proc type information from the corresponding config
     proc = read(module['type_data']['proc'],'proc')
-    results = build_module_entry(module,module_num)
-    self.assertTrue(results['proc_type_data']['id']=='DCLEG')
+    build_module_entry(module,module_num)
+    self.assertTrue(module['proc_type_data']['id']=='GLG')
     # once we have the proc type config data, it must by converted into unified proc groups
-    self.assertTrue(len(results['proc_groups'])==17)
+    self.assertTrue(len(module['proc_groups'])==7)#17)
     # once we have the proc type config
-    for i,stage in enumerate(results['stages']):
+    for i,stage in enumerate(module['stages']):
       self.assertTrue(stage == (proc['stage_to_groups_dict'][i]))
     # we must check that each group is created, and populated with the correct details
-    for i,group_key in enumerate(results['proc_groups']):
-
+    for i,group_key in enumerate(module['proc_groups']):
       # we must check that the group has the correct location,positions data
-      self.assertEqual(results['proc_groups'][group_key]['pos_data']['x'],0)
+      self.assertEqual(module['proc_groups'][group_key]['pos_data']['x'],0)
 
       # we must check that the inputs have been added
       if module['inputs'] is not None:
-        self.assertTrue(results['proc_groups'][group_key]['inputs'].contains(module['inputs']))
+         self.assertTrue(all(elem in module['inputs'] for elem in module['inputs']))
       for i,inp_key in enumerate(proc['inputs']):
         if proc['inputs'][inp_key] is not None:
-          self.assertTrue(results['proc_groups'][group_key]['inputs'].contains(proc['inputs'][inp_key]))
+          self.assertTrue(all(elem in module['inputs'] for elem in proc['inputs'][inp_key]))
 
       # we must check that the outputs have been added
       if module['outputs'] is not None:
-        self.assertTrue(results['proc_groups'][group_key]['outputs'].contains(module['outputs']))
+        self.assertTrue(all(elem in module['outputs'] for elem in module['outputs']))
       for i,out_key in enumerate(proc['outputs']):
         if proc['outputs'][out_key] is not None:
-          self.assertTrue(results['proc_groups'][group_key]['outputs'].contains(proc['outputs'][out_key]))
+          self.assertTrue(all(elem in module['outputs'] for elem in proc['outputs'][out_key]))
 
       # # we must check that the hooks have been added
       # if ('hooks_into' in proc and group_key in proc['hooks_into']):
-      #   self.assertTrue(results['proc_groups'][group_key]['hooks_into'] is None)
+      #   self.assertTrue(module['proc_groups'][group_key]['hooks_into'] is None)
       # else:
-      #   self.assertTrue(results['proc_groups'][group_key]['hooks_into'].contains(proc['hooks_into'][group_key]))
+      #   self.assertTrue(module['proc_groups'][group_key]['hooks_into'].contains(proc['hooks_into'][group_key]))
       # if ('hooks_outof' in proc and group_key in proc['hooks_outof']):
-      #   self.assertTrue(results['proc_groups'][group_key]['hooks_outof'] is None)
+      #   self.assertTrue(module['proc_groups'][group_key]['hooks_outof'] is None)
       # else:
-      #   self.assertTrue(results['proc_groups'][group_key]['hooks_outof']==proc['hooks_outof'][group_key])
+      #   self.assertTrue(module['proc_groups'][group_key]['hooks_outof']==proc['hooks_outof'][group_key])
       
     # we must check that the hooks have been added
     if ('hooks_into' in proc):
-      self.assertTrue(results['hooks_into']==proc['hooks_into'])
+      self.assertTrue(module['hooks_into']==proc['hooks_into'])
     else:
-      self.assertTrue(results['hooks_into'] is None)
+      self.assertTrue(module['hooks_into'] is None)
     if ('hooks_outof' in proc):
-      self.assertTrue(results['hooks_outof']==proc['hooks_outof'])
+      self.assertTrue(module['hooks_outof']==proc['hooks_outof'])
     else:
-      self.assertTrue(results['hooks_outof'] is None)
+      self.assertTrue(module['hooks_outof'] is None)
 
     # we must check that the links defined have been added
     if (proc['links_defined'] is None):
-      self.assertTrue(results['links_defined'] is None)
+      self.assertTrue(module['links_defined'] is None)
     else:
-      self.assertTrue(results['links_defined']== proc['links_defined'])
+      self.assertTrue(module['links_defined']== proc['links_defined'])
     # we must check that the links used have been added
     if (proc['links_used'] is None):
-      self.assertTrue(results['links_used'] is None)
+      self.assertTrue(module['links_used'] is None)
     else:
-      self.assertTrue(results['links_used'] == proc['links_used'])
+      self.assertTrue(module['links_used'] == proc['links_used'])
 
 
 if __name__ == '__main__':
--- utils/config_operations.py
+++ utils/config_operations.py
@@ -2,34 +2,35 @@ from utils.config_reader import read
 
 def build_full_config(config):
     for i,module in enumerate(config['modules']):
-        entry = build_module_entry(module,i)
-        config['modules'][i]=entry
-    return module
+        build_module_entry(module,i)
+        config['modules'][i]=module
+    return config
 
 def build_module_entry(module,m):
     proc = read(module['type_data']['proc'],'proc')
-    result = module
-    result['proc_type_data'] = proc['type_data']
-    result['stages'] = []
-    result['proc_groups'] = dict()
+    module['proc_type_data'] = proc['type_data']
+    module['stages'] = []
+    module['proc_groups'] = dict()
     for i,stage in enumerate(proc['stage_to_groups_dict']):
-        # result['stages'][[stage['id']] = stage
-        result['stages'].append(stage)
+        # module['stages'][[stage['id']] = stage
+        module['stages'].append(stage)
         for group in stage['groups']:
-            result['proc_groups'][group]={
+            module['proc_groups'][group]={
                     'id':group,
                     'pos_data':{'x':0,'y':m,'z':i}
                 }
-    for i,group in enumerate(proc['group_details']):
-        result['proc_groups'][group['id']].update(group)
-    for i,group in enumerate(proc['inputs']):
-        if (proc['inputs'][group] is not None):
-            result['inputs'].update(proc['inputs'][group])
-    for i,group in enumerate(proc['outputs']):
-        if (proc['outputs'][group] is not None):
-            result['outputs'].update(proc['outputs'][group])
-    result['hooks_into']=proc['hooks_into']
-    result['hooks_outof']=proc['hooks_outof']
-    result['links_defined']=proc['links_defined']
-    result['links_used']=proc['links_used']
-    return result
+    if proc['group_details'] is not None:
+        for i,group in enumerate(proc['group_details']):
+            module['proc_groups'][group['id']].update(group)
+    if proc['inputs'] is not None:
+        for i,group in enumerate(proc['inputs']):
+            if (proc['inputs'][group] is not None):
+                module['inputs'].extend(proc['inputs'][group])
+    if proc['outputs'] is not None:
+        for i,group in enumerate(proc['outputs']):
+            if (proc['outputs'][group] is not None):
+                module['outputs'].extend(proc['outputs'][group])
+    module['hooks_into']=proc['hooks_into']
+    module['hooks_outof']=proc['hooks_outof']
+    module['links_defined']=proc['links_defined']
+    module['links_used']=proc['links_used']
