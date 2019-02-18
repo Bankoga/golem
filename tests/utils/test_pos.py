66abd4be6fe278f6077bc2497e3cb29c513cdc03
v <bankoga@gmail.com>
2019-02-18 10:19:56 -0600

    test implemented pos init

--- tests/utils/test_pos.py
+++ tests/utils/test_pos.py
@@ -1,6 +1,21 @@
 import unittest
+from hypothesis import given
+from hypothesis import strategies as st
+from utils.pos import Pos
 
 class TestPos(unittest.TestCase):
   
-  def test_init(self):
-    pass
+  @given(st.integers(),st.integers(),st.integers(),st.integers())
+  def test_init_with_data(self,s,x,y,z):
+    pos = Pos(s,x,y,z)
+    self.assertEqual(pos.s,s)
+    self.assertEqual(pos.x,x)
+    self.assertEqual(pos.y,y)
+    self.assertEqual(pos.z,z)
+  
+  def test_init_without_data(self):
+    pos = Pos()
+    self.assertEqual(pos.s,-1)
+    self.assertEqual(pos.x,-1)
+    self.assertEqual(pos.y,-1)
+    self.assertEqual(pos.z,-1)
--- utils/pos.py
+++ utils/pos.py
@@ -1,3 +1,6 @@
 class Pos:
-  def __init__(self):
-    pass
+  def __init__(self,s=-1,x=-1,y=-1,z=-1):
+    self.s = s
+    self.x = x
+    self.y = y
+    self.z = z
