e9994f75778362384fa9e655e2792a87008da026
v <bankoga@gmail.com>
2019-02-19 13:35:12 -0600

    commented on module purpose reworked build func groups

--- components/module.py
+++ components/module.py
@@ -1,7 +1,11 @@
-from utils.func_group import FuncGroup
+from components.func_groups.func_group import FuncGroup
 
 class Module():
 
+  """
+  Each module serves as a discrete env, and datapack repository for a set of func groups
+  Responsible for aggregating datapacks with the same keys into single datapacks for routing
+  """
   def __init__(self, proc):
     # once fully initialized, a module has very few aggregate properties to consider during operation
     self.proc = proc
@@ -12,7 +16,8 @@ class Module():
 
   def _build_func_groups_(self):
     for group_id in self.proc.groups:
-      self.groups[group_id] = FuncGroup(self.proc.groups[group_id])
+      group = self.proc.groups[group_id]
+      self.groups[group_id] = FuncGroup(group, group.type)
       pass
 
   # def operate(self,inputShapes):
