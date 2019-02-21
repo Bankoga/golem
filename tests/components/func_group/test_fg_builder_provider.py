7560efae397cda2e301ac19cd96960bfa0397029
v <bankoga@gmail.com>
2019-02-21 09:40:30 -0600

    Cleaned up naming in fg builder provider

--- tests/components/func_group/test_fg_builder_provider.py
+++ tests/components/func_group/test_fg_builder_provider.py
@@ -8,8 +8,8 @@ from  data.axioms.enums import FuncGroup
 class TestFGBuilderProvider(unittest.TestCase):
   @given(st.sampled_from(sorted(FuncGroup)))
   def test_get(self, fg_type_id):
-    ordinator = fg_services.get(fg_type_id, **{})
-    self.assertTrue(ordinator.get_id(), fg_type_id)
+    fg = fg_services.get(fg_type_id, **{})
+    self.assertTrue(fg.get_id(), fg_type_id)
     
 if __name__ == '__main__':
     unittest.main()
