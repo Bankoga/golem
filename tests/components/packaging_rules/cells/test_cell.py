9d710d2838471ead6eb566d3a7f23110ea13637a
v <bankoga@gmail.com>
2019-02-23 12:17:01 -0600

    began work on specifying how cells pack inputs into outputs

--- tests/components/packaging_rules/cells/test_cell.py
+++ tests/components/packaging_rules/cells/test_cell.py
@@ -5,7 +5,9 @@ from hypothesis import strategies as st
 
 from components.packaging_rules.cells.cell import Cell
 from data.axioms.cell_types import CellType,cell_data
-from data.axioms.enums import RuleType
+from data.axioms.enums import RuleType, RsrcType, PackType, FieldType
+
+from utils.helpers.address_help import build_address, build_meld, build_datapack
 
 class TestCell(unittest.TestCase):
   # def setUp(self):
@@ -37,8 +39,36 @@ class TestCell(unittest.TestCase):
     self.assertEqual(cell.pct_of_pod, type_data['pct_of_pod'])
     self.assertEqual(cell.init_threshhold, type_data['init_threshhold'])
     self.assertEqual(cell.activation_function, type_data['activation_function'])
-    # self.assertEqual(cell, type_data[])
+  
+  @given(st.data())
+  def test_pack(self,inputs):
+    """
+    but I need a guaranteed order to some of the inputs don't I?
+    Why? Bc agg type datapacks get combined in a spatially oriented way
+    Actually, bc of position data that is embedded in each sender we have a way to guarantee order of processing
+    Given that each sender has a position, this can be added to the datapack along with sender address (or in place of?)
+    Given that we are sent inputs
+    When we prepare to evaluate them
+    Then we sort them using a guaranteed sort by pos first!
 
+    Where does this sort live? In utils.helpers.pos_help?
+
+    Specifiying inputs for testing datapacks, and things that rely on them is growing more complex
+    At this point, I think it makes the most sense to begin work on a datapack series of custom hypothesis strategies
+
+    aggregate datapacks can't exist or not exist as they please
+    they must always exist in the correct order to be processed
+    Thus we assume that an order id list or dict has been generated which we can compare against
+    """
+    # build a
+    st.lists(st.builds(build_datapack, st.sampled_from(['SenderModuleId','self','Self']),
+        st.sampled_from(['sender_group_id','self','Self', '']),
+        st.sampled_from(RsrcType),
+        st.sampled_from(PackType),
+        st.sampled_from(FieldType),
+        st.sampled_from(['SenderModuleId','self','Self']),
+        st.sampled_from(['sender_group_id','self','Self',''])))
+    pass
 
 if __name__ == '__main__':
   unittest.main()
