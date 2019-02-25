873aabbe40ba186f2011fba80a0ce334f1d68fcf
v <bankoga@gmail.com>
2019-02-25 08:49:25 -0600

    worked some on conv helper

--- tests/utils/helpers/test_convoluter.py
+++ tests/utils/helpers/test_convoluter.py
@@ -3,8 +3,16 @@ import unittest
 from hypothesis import given
 from hypothesis import strategies as st
 
+from utils.helpers.convoluter import get_conv_sign
+
 class TestConvoluter(unittest.TestCase):
   pass
 
+  def test_get_conv_sign(self, inp_shape, out_shape):
+    if the shapes are not convolvable
+      with self.assertRaises()
+    if the input matrix is larger than the output matrix
+    elif the input matrix is smaller than the output matrix
+    else
 if __name__ == '__main__':
   unittest.main()
--- utils/helpers/convoluter.py
+++ utils/helpers/convoluter.py
@@ -1,11 +1,14 @@
-from chainer import Variable
+from utils.shape import Shape
 
 """
 A repository of helpers for the atypical convs used here
 """
 
 
-  def get_conv_sign(inp_shape, out_shape):
-    if inp:
-    elif the input matrix is smaller than the output matrix:
-    else: 
+def get_conv_sign(inp_shape, out_shape):
+  conv_sign = 0
+  if inp_shape > out_shape:
+    conv_sign = -1
+  elif inp_shape < out_shape:
+    conv_sign = 1
+  return conv_sign
