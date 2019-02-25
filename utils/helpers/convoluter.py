6aea7fc80fd43e3e120003b045967adc3aad0a5f
v <bankoga@gmail.com>
2019-02-25 08:49:13 -0600

    added a convolutions helper

---
+++ utils/helpers/convoluter.py
@@ -0,0 +1,11 @@
+from chainer import Variable
+
+"""
+A repository of helpers for the atypical convs used here
+"""
+
+
+  def get_conv_sign(inp_shape, out_shape):
+    if inp:
+    elif the input matrix is smaller than the output matrix:
+    else: 
