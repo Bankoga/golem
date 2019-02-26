e448905c7c4be747c154ccdaa16ff5c092aae33c
v <bankoga@gmail.com>
2019-02-26 11:18:43 -0600

    added valid pos to base instruction

--- tests/utils/test_instruction.py
+++ tests/utils/test_instruction.py
@@ -5,24 +5,28 @@ from hypothesis import strategies as st
 
 from data.axioms.enums import RuleType
 from tests.strategies.enum_strats import ruletype
+from tests.strategies.pos_strats import valid_pos
 
 from utils.pos import Pos
 from utils.instruction import Instruction
 
 class TestInstruction(unittest.TestCase):
   def setUp(self):
-    self.instruction = Instruction(RuleType.CELL)
+    # self.instruction = Instruction(RuleType.CELL, )
+    pass
 
-  @given(ruletype()) # pylint: disable=no-value-for-parameter
-  def test_default(self, rtype):
-    inst = Instruction(rtype)
+  @given(ruletype(), valid_pos()) # pylint: disable=no-value-for-parameter
+  def test_default(self, rtype, pos):
+    # for efficiency reasons, eventually instructions will need to be built before processing
+    inst = Instruction(rtype, pos)
     self.assertEqual(inst.type, rtype)
     self.assertIsNone(inst.curr_shape)
     self.assertIsNone(inst.curr_bearing)
     self.assertIsNone(inst.curr_pos)
+    self.assertEqual(inst.pos,pos)
 
-  def test_perform(self, package):
-    pass
+  # def test_perform(self, package):
+  #   pass
 
 if __name__ == '__main__':
   unittest.main()
--- utils/instruction.py
+++ utils/instruction.py
@@ -1,11 +1,12 @@
 from abc import abstractmethod
 
 class Instruction:
-  def __init__(self, rtype):
+  def __init__(self, rtype, pos):
     self.type = rtype
     self.curr_shape = None
     self.curr_bearing = None
     self.curr_pos = None
+    self.pos = pos
 
   # for nested cardinal rotations, apply each rotation by its value/the number of rotations
   @abstractmethod
