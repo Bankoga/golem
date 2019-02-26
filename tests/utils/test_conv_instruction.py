42ccfaa555fea7d47e0f8a2aacdc6ab749dde946
v <bankoga@gmail.com>
2019-02-26 11:22:01 -0600

    added pos to conv instruction

--- tests/utils/test_conv_instruction.py
+++ tests/utils/test_conv_instruction.py
@@ -5,7 +5,8 @@ from hypothesis import strategies as st
 
 from data.axioms.enums import RuleType
 from tests.strategies.enum_strats import ruletype
-from tests.strategies.packing_strats import valid_directions, valid_shapes
+from tests.strategies.packing_strats import valid_shapes
+from tests.strategies.pos_strats import valid_directions, valid_pos
 
 from utils.pos import Pos
 from utils.conv_instruction import ConvInstruction
@@ -15,18 +16,25 @@ class TestConvInstruction(unittest.TestCase):
     # self.inst = ConvInstruction(RuleType.CELL)
     pass
 
-  @given(valid_directions(), valid_shapes()) # pylint: disable=no-value-for-parameter
-  def test_default(self, directions, shapes):
-    inst = ConvInstruction(directions, shapes)
+  @given(valid_directions(), valid_shapes(),valid_pos()) # pylint: disable=no-value-for-parameter
+  def test_default(self, directions, shapes, pos):
+        # for efficiency reasons, eventually instructions will need to be built before processing
+    inst = ConvInstruction(directions, shapes, pos)
     self.assertEqual(inst.type, RuleType.CELL)
     self.assertIsNone(inst.curr_shape)
     self.assertIsNone(inst.curr_bearing)
     self.assertIsNone(inst.curr_pos)
     self.assertEqual(inst.directions,directions)
     self.assertEqual(inst.shapes,shapes)
+    self.assertEqual(inst.pos,pos)
 
   def test_perform(self, package):
-    pass
+    """
+    for each direction
+      take a sample of the package for each shape
+      record activity information while sampling
+    combine all samples into a single result using distance attenuation
+    """
 
 if __name__ == '__main__':
   unittest.main()
--- utils/conv_instruction.py
+++ utils/conv_instruction.py
@@ -3,8 +3,8 @@ from data.axioms.enums import RuleType
 from utils.instruction import Instruction
 
 class ConvInstruction(Instruction):
-  def __init__(self,directions,shapes):
-    super().__init__(RuleType.CELL)
+  def __init__(self,directions,shapes, pos):
+    super().__init__(RuleType.CELL, pos)
     self.directions = directions
     self.shapes = shapes
     
