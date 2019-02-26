ce6119f4895753274816e409a8161a4fb42f1d63
v <bankoga@gmail.com>
2019-02-26 11:32:42 -0600

    changed some imports in a comment

--- tests/components/func_group/test_fg_builder.py
+++ tests/components/func_group/test_fg_builder.py
@@ -3,11 +3,11 @@
 # from hypothesis import strategies as st
 # import utils.object_factory
 # from components.ordinators.ordinator import Ordinator
-# from data.axioms.pos_maps import direction_keys
+# from data.axioms.pos_maps import ordinal_keys
 
 # class TestOrdinator(unittest.TestCase):
   
-#   @given(st.sampled_from(sorted(direction_keys)))
+#   @given(st.sampled_from(sorted(ordinal_keys)))
 #   def test_init(self, arbitrary_direction):
 #     ordinator = Ordinator(arbitrary_direction)
 #     self.assertTrue(ordinator.get_id(), arbitrary_direction)
