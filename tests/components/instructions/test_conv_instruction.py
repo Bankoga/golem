1431d037a4d7b8c2cf40d7b14120d44c717a5e5b
v <bankoga@gmail.com>
2019-02-27 10:30:23 -0600

    updated expectation of weight initialization outcome

--- tests/components/instructions/test_conv_instruction.py
+++ tests/components/instructions/test_conv_instruction.py
@@ -11,6 +11,7 @@ from tests.strategies.pos_strats import valid_direction, valid_pos
 
 from utils.pos import Pos
 from components.instructions.conv_instruction import ConvInstruction
+from numpy import ones, array_equal
 
 class TestConvInstruction(unittest.TestCase):
   def setUp(self):
@@ -32,7 +33,10 @@ class TestConvInstruction(unittest.TestCase):
   def test_set_up_weights(self,shapes):
     weights = self.inst.set_up_weights(shapes)
     for shape in shapes:
-      self.assertEqual(weights[shape].shape, shape)
+      expectation = ones(shape)
+      result = weights[shape]
+      self.assertEqual(result.shape, shape)
+      self.assertTrue(array_equal(result, expectation))
 
   @given(module_input_set()) # pylint: disable=no-value-for-parameter
   def test_perform(self, inputs):
