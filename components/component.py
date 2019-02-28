5e7c7fa67cedf4bc08011c772a09b2ef435d2e9f
v <bankoga@gmail.com>
2019-02-28 13:17:26 -0600

    added param for id to component

--- components/component.py
+++ components/component.py
@@ -2,12 +2,12 @@ from data.enums.pos import ComponentType
 
 class Component:
   # TODO: implement a base class that is shared across all components
-  def __init__(self, component_id,component_type):
+  def __init__(self, component_id,component_type, id=None):
     """
     component_id : the semantic key used as reference for the component object
     component_type : the semantic key used as reference for the component functionality within the matrix
     """
-    self.id = None
+    self.id = id
     self.c_id = component_id
     self.c_type = ComponentType(component_type)
     self.op_lvl = self.c_type.value
