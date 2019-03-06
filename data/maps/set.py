21c51d54cab62212db63e99d4799ecc6925d9346
v <bankoga@gmail.com>
2019-03-06 09:11:27 -0600

    converted get ids to use super set instead of set

--- data/maps/set.py
+++ data/maps/set.py
@@ -1,13 +1,13 @@
 from data.axioms.configs import coder_ids, proc_ids, set_ids
-from data.enums.prop_types import SetType
+from data.enums.prop_types import SuperSet
 
 def get_ids(set_type):
-  if set_type is None or set_type is SetType.UNSET:
+  if set_type is None or set_type is SuperSet.UNSET:
     raise ValueError('Supplied Invalid Set Type')
   else:
-    if 100 < set_type.value and set_type.value < 200:
+    if set_type is SuperSet.CODER:# if 100 < set_type.value and set_type.value < 200:
       result = list(coder_ids.values())
-    elif 200 < set_type.value and set_type.value < 300:
+    elif set_type is SuperSet.PROC:#elif 200 < set_type.value and set_type.value < 300:
       result = list(proc_ids.values())
     else:
       result = []
