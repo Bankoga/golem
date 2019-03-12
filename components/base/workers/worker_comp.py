154ea9931b5db82baed0903851e53ebef02f4a1e
v <bankoga@gmail.com>
2019-03-12 10:13:35 -0500

    Stub implemented worker comp as a static comp

--- components/base/workers/worker_comp.py
+++ components/base/workers/worker_comp.py
@@ -1,5 +1,5 @@
 from components.base.static_comp import StaticComp
 
 class WorkerComp(StaticComp):
-  def __init__(self):
-    pass
+  def __init__(self, **kwargs):
+    super().__init__(kwargs['item_id'],kwargs['ctg'])
---
+++ tests/components/base/workers/test_worker_comp.py
@@ -0,0 +1,18 @@
+import unittest
+
+from hypothesis import given
+from hypothesis import strategies as st
+
+from components.base.workers.worker_comp import WorkerComp
+from components.enums.pos import CtgType
+from tests.components.base.test_static_comp import TestStaticComp
+
+class TestWorkerComp(TestStaticComp):
+  def setUp(self):
+    self.label = 'TotallyValidId'
+    self.ctg = CtgType.FSET
+    self.comp = WorkerComp(item_id=self.label, ctg=self.ctg)
+
+
+if __name__ == '__main__':
+  unittest.main()
