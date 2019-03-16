e3a98f16eff7972ed3b503cd40f733ad26f736d1
v <bankoga@gmail.com>
2019-03-15 19:03:54 -0500

    reworded comment

--- components/base/workers/mediator_comp.py
+++ components/base/workers/mediator_comp.py
@@ -1,1 +1,1 @@
-# TODO: every mediator needs to receive a hook to the registry so that it can be used to send packages and register things
+# TODO: every mediator needs to receive a hook to the registry so that it can be used to create channels and register things
