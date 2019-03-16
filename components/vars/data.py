1f516fe47047bc4f3cf8a568432ab1a1aa494fda
v <bankoga@gmail.com>
2019-03-16 10:05:54 -0500

    paused work on locales added address to data vars

--- components/vars/data.py
+++ components/vars/data.py
@@ -1,7 +1,34 @@
 import numpy
 import typing
 
+from utils.pos import Pos
+
 locale = ('address', 'pos')
+
+class Address(typing.NamedTuple):
+  golem: str
+  matrix: str
+  func_set: str
+  stage: str
+  group: str
+  packager: str
+  instruction: str
+  # items must be filled in the correct order without skipping!
+
+# class Graph(typing.NamedTuple):
+#   space: type of space
+#   points: set of points in the space
+#   edges: list of edges between points
+
+# class DataLocale(typing.NamedTuple):
+#   pass
+
+# class WorkerLocale(typing.NamedTuple):
+#   addr: Address
+
+# class MediatorLocale(WorkerLocale):
+#   incoming: list
+#   outgoing: list
 
 class ConvVar(typing.NamedTuple):
   source_id: str
