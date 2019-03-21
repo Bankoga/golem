dd658db4aa97b3da8eb59399af03d28821eca961
v <bankoga@gmail.com>
2019-03-21 14:25:27 -0500

    removed some unnecessary props from instruction

--- components/instructions/instruction.py
+++ components/instructions/instruction.py
@@ -4,15 +4,12 @@ from components.enums.pos import CtgType
 
 class Instruction(ConsumerComp):
   """
-  Instruction: An algorithm for turning a specific set of inputs into outputs for a channel
+  Instruction: An algorithm for turning a buffer of inputs into outputs for a channel
   """
   def __init__(self, *args, **kwargs):
     if not 'label' in kwargs and not kwargs['label'] is None:
       kwargs['label'] = f'{args[0].name}-{hash(args[1])}'
     kwargs['ctg'] = CtgType.INSTRUCTION
     super().__init__(*args, **kwargs)
-    self.curr_shape = None
-    self.curr_bearing = None
-    self.curr_pos = None
     
   # for nested cardinal rotations, apply each rotation by its value/the number of rotations
