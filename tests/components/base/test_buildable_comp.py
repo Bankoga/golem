b6654c695f9c35f6a272b555d5ffd99af5731f34
v <bankoga@gmail.com>
2019-03-21 09:32:11 -0500

    reworked buildable tests

--- tests/components/base/test_buildable_comp.py
+++ tests/components/base/test_buildable_comp.py
@@ -14,27 +14,25 @@ class TestBuildableComp(TestPassiveComp):
     self.values = []
     self.var = tuple(self.values)
     self.comp = BuildableComp(label=self.label, ctg=self.ctg)
+    self.comp.build(*self.values)
 
   def test_set_is_built(self):
+    self.comp = BuildableComp(label=self.label, ctg=self.ctg)
     with self.assertRaises(RuntimeError):
       self.comp.is_built = 'Does not matter'
 
   def test_get_var(self):
-    self.comp.build(*self.values)
     self.assertEqual(self.comp.var, self.var)
 
   def test_build(self):
-    self.comp.build(*self.values)
     self.assertEqual(self.comp.var, self.var)
     self.assertTrue(self.comp.is_built)
 
   def test_double_build(self):
-    self.comp.build(*self.values)
     with self.assertRaises(RuntimeError):
       self.comp.build(*self.values)
 
   def test_set_built_var(self):
-    self.comp.build(*self.values)
     with self.assertRaises(RuntimeError):
       self.comp.var = self.var
 
