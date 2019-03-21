ee872f323e093e2c364abd90a487de5298eb4f8d
v <bankoga@gmail.com>
2019-03-21 13:49:32 -0500

    refactored instruction into proper consumer comp

--- components/instructions/instruction.py
+++ components/instructions/instruction.py
@@ -1,33 +1,18 @@
-from abc import abstractmethod
 from components.base.workers.consumer_comp import ConsumerComp
 from components.enums.prop_types import RuleType
+from components.enums.pos import CtgType
 
 class Instruction(ConsumerComp):
-  def __init__(self, label, rtype, pos):
-    self.item_label = f'{rtype.name}-{pos.get_hash()}'
-    # hash(self.item_label)
-    # What is the id of AN instruction in the matrix?
-    # some func of type name, shape, position, and ?
-    super().__init__(label, rtype.get_component_type(), rtype)
+  """
+  Instruction: An algorithm for turning a specific set of inputs into outputs for a channel
+  """
+  def __init__(self, *args, **kwargs):
+    if not 'label' in kwargs and not kwargs['label'] is None:
+      kwargs['label'] = f'{args[0].name}-{hash(args[1])}'
+    kwargs['ctg'] = CtgType.INSTRUCTION
+    super().__init__(*args, **kwargs)
     self.curr_shape = None
     self.curr_bearing = None
     self.curr_pos = None
-    self.pos = pos
     
   # for nested cardinal rotations, apply each rotation by its value/the number of rotations
-  @abstractmethod
-  def perform(self,inputs):
-    pass
-  
-  @abstractmethod
-  def set_up_weights(self, shapes):
-    pass
-  
-  def build(self,data=None):
-    super().build(data)
-  
-  def reset(self):
-    pass
-
-  def update(self, new_data):
-    super().update(new_data)
--- tests/components/instructions/test_instruction.py
+++ tests/components/instructions/test_instruction.py
@@ -2,47 +2,62 @@ import unittest
 
 from hypothesis import given
 from hypothesis import strategies as st
-
-from components.enums.prop_types import RuleType
-from components.enums.pos import CtgType
-from tests.strategies.prop_strats import rule_type_prop, arbitrary_id
-from tests.strategies.pos_strats import valid_pos
-from tests.strategies.data_strats import valid_resource_data
-
-from utils.pos import Pos
-from components.instructions.instruction import Instruction
-
-from tests.components.base.test_component import TestComponent
-
 from numpy import array_equal
 
-class TestInstruction(TestComponent):
+from components.enums.pos import CtgType
+from components.enums.prop_types import RuleType
+from components.instructions.instruction import Instruction
+from components.matrix.address_registry import AddressRegistry
+from components.vars.data import Address
+from tests.components.base.workers.test_consumer_comp import TestConsumerComp
+from tests.strategies.data_strats import valid_resource_data
+from tests.strategies.pos_strats import valid_pos
+from tests.strategies.prop_strats import arbitrary_id, rule_type_prop
+from utils.pos import Pos
+
+class TestInstruction(TestConsumerComp):
+  def set_up_base(self):
+    self.label = 'TotallyValidId'
+    self.ctg = CtgType.INSTRUCTION
+
+  def set_up_var(self):
+    self.registry = AddressRegistry(label='global_address_registry_api')
+    self.address = Address(golem='a',matrix='l',func_set='b', stage='a',group='a',packager='p',instruction=self.label)
+    self.reg_item = {
+      'reg_id': self.label,
+      'address': self.address
+    }
+    self.rule_type = RuleType.CONV
+    self.values = [self.registry, self.rule_type]
+    self.var = tuple(self.values)
+
   def setUp(self):
-    self.valid_c_id = 'TotallyValidId'
-    self.valid_c_type = RuleType.CONV
-    self.pos = Pos(RuleType.CONV.get_component_type())
-    self.comp = Instruction(self.valid_c_id,self.valid_c_type,self.pos)
+    self.set_up_base()
+    self.set_up_var()
+    # self.pos = Pos(RuleType.CONV.get_component_type())
+    self.comp = Instruction(label=self.label)
+    self.comp.build(*self.values)
 
-  @given(arbitrary_id(), rule_type_prop(), valid_pos()) # pylint: disable=no-value-for-parameter
-  def test_default(self, label, rtype, pos):
-    # for efficiency reasons, eventually instructions will need to be built before processing
-    # label = f'{rtype.name}-{pos.get_hash()}' # What is the id of AN instruction in the matrix?
-    inst = Instruction(label, rtype, pos)
-    c_lvl = rtype.get_component_type()
-    self.assertEqual(inst.get_id(), label)
-    self.assertEqual(inst.op_lvl, CtgType(c_lvl))
-    self.assertEqual(inst.ctg_type, rtype)
-    self.assertIsNone(inst.curr_shape)
-    self.assertIsNone(inst.curr_bearing)
-    self.assertIsNone(inst.curr_pos)
-    self.assertEqual(inst.pos,pos)
+  # @given(arbitrary_id(), rule_type_prop(), valid_pos()) # pylint: disable=no-value-for-parameter
+  # def test_default(self, label, rtype, pos):
+  #   # for efficiency reasons, eventually instructions will need to be built before processing
+  #   # label = f'{rtype.name}-{pos.get_hash()}' # What is the id of AN instruction in the matrix?
+  #   inst = Instruction(label, rtype, pos)
+  #   c_lvl = rtype.get_component_type()
+  #   self.assertEqual(inst.label, label)
+  #   self.assertEqual(inst.op_lvl, CtgType(c_lvl))
+  #   self.assertEqual(inst.ctg_type, rtype)
+  #   self.assertIsNone(inst.curr_shape)
+  #   self.assertIsNone(inst.curr_bearing)
+  #   self.assertIsNone(inst.curr_pos)
+  #   self.assertEqual(inst.pos,pos)
 
-  def test_build(self):
-    comp = Instruction(self.valid_c_id, self.valid_c_type,self.pos)
-    self.assertFalse(comp.is_built())
-    comp.build()
-    self.assertTrue(comp.is_built())
-    comp.operate()
+  # def test_build(self):
+  #   comp = Instruction(self.label, self.ctg,self.pos)
+  #   self.assertFalse(comp.is_built())
+  #   comp.build()
+  #   self.assertTrue(comp.is_built())
+  #   comp.operate()
 
 if __name__ == '__main__':
   unittest.main()
