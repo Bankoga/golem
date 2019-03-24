b697b57845f94e95e2a760c22abd86f65bf4f50f
v <bankoga@gmail.com>
2019-03-24 13:09:42 -0500

    worked on weighted comp - added weights, num_dim_of_mass, is_locked, and shape - added locking mechanism

--- components/base/weighted_comp.py
+++ components/base/weighted_comp.py
@@ -4,3 +4,55 @@ class WeightedComp(StaticComp):
 
   def __init__(self, *args, **kwargs):
    super().__init__(kwargs['label'], kwargs['ctg'])
+   self.set_defaults()
+  
+  def set_defaults(self):
+    self.__is_locked = False
+    self.__num_dim_of_mass = 0
+    self.__shape = None
+    self.__weights = []
+   
+  @property
+  def weights(self):
+    return self.__weights
+  
+  @weights.setter
+  def weights(self, value):
+    if self.is_locked:
+      raise RuntimeError('Can not change weights of a locked component!')
+    else:
+      self.__weights = value
+    
+  @property
+  def shape(self):
+    return self.__shape
+  
+  @shape.setter
+  def shape(self, value):
+    if self.is_locked:
+      raise RuntimeError('Can not change shape of a locked component!')
+    else:
+      self.__shape = value
+      self.__num_dim_of_mass = len(self.__shape)
+    
+  @property
+  def num_dim_of_mass(self):
+    return self.__num_dim_of_mass
+  
+  @num_dim_of_mass.setter
+  def num_dim_of_mass(self, value):
+    raise RuntimeError('Can not set the num dim of mass! It is derived from the shape!')
+  
+  @property
+  def is_locked(self):
+    return self.__is_locked
+  
+  @is_locked.setter
+  def is_locked(self, value):
+    raise RuntimeError('Toggle the lock status via toggle lock instead!')
+  
+  def toggle_lock(self):
+    if self.is_locked:
+      self.__is_locked = False
+    else:
+      self.__is_locked = True
--- tests/components/base/test_weighted_comp.py
+++ tests/components/base/test_weighted_comp.py
@@ -7,15 +7,70 @@ from components.base.weighted_comp import WeightedComp
 from components.enums.pos import CtgType
 
 from tests.components.base.test_static_comp import TestStaticComp
+from tests.strategies.data_strats import valid_shape, valid_weights
+from numpy import array_equal
 
 class TestWeightedComp(TestStaticComp):
   def set_up_base(self):
     self.label = 'TotallyValidId'
     self.ctg = CtgType.INSTRUCTION
+
+  def set_up_defaults(self):
+    self.default_num_dim_of_mass = 0
+    self.default_shape = None
+    self.default_weights = []
+    self.default_is_locked = False
+
+  def set_up_dynamic_props(self):
+    pass
   
   def setUp(self):
     self.set_up_base()
+    self.set_up_defaults()
+    self.set_up_dynamic_props()
     self.comp = WeightedComp(label=self.label, ctg=self.ctg)
+
+  def test_get_num_dim_of_mass(self):
+    self.assertTrue(array_equal(self.comp.num_dim_of_mass, self.default_num_dim_of_mass))
+  
+  def test_set_num_dim_of_mass(self):
+    with self.assertRaises(RuntimeError):
+      self.comp.num_dim_of_mass = self.default_num_dim_of_mass
+
+  def test_get_shape(self):
+    self.assertTrue(array_equal(self.comp.shape, self.default_shape))
+  
+  @given(valid_shape()) # pylint: disable=no-value-for-parameter
+  def test_set_shape(self, new_shape):
+    self.comp.shape = new_shape
+    self.assertTrue(array_equal(self.comp.shape, new_shape))
+    self.assertTrue(array_equal(self.comp.num_dim_of_mass, len(new_shape)))
+
+  def test_get_weights(self):
+    self.assertTrue(array_equal(self.comp.weights, self.default_weights))
+  
+  @given(valid_weights()) # pylint: disable=no-value-for-parameter
+  def test_set_weights(self, new_weights):
+    self.comp.weights = new_weights
+    self.assertTrue(array_equal(self.comp.weights, new_weights))
+
+  def test_get_is_locked(self):
+    self.assertTrue(array_equal(self.comp.is_locked, self.default_is_locked))
+  
+  def test_set_is_locked(self):
+    with self.assertRaises(RuntimeError):
+      self.comp.is_locked = True
+
+  def test_lock_weights(self):
+    self.comp.toggle_lock()
+    self.assertTrue(self.comp.is_locked)
+    with self.assertRaises(RuntimeError):
+      self.comp.weights = self.default_weights
+    with self.assertRaises(RuntimeError):
+      self.comp.shape = self.default_shape
+
+
+
 
 if __name__ == '__main__':
   unittest.main()
