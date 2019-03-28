03d0a159a0f88daea3598d8a78abc9bdf3d504a5
v <bankoga@gmail.com>
2019-03-28 09:50:04 -0500

    added address and source index to cllct seg

--- components/data/collector_segment.py
+++ components/data/collector_segment.py
@@ -12,15 +12,18 @@ class CollectorSegment(PlasticComp):
   the position is relative to its parents X,Y (or row/column) index within stage Z
   """
 
-  def __init__(self,f_shape, **kwargs):
+  def __init__(self,address,source_index,f_shape, **kwargs):
     has_label = ('label' in kwargs and not kwargs['label'] is None)
     if not has_label:
       kwargs['label'] = ''
     kwargs['ctg']=CtgType.DATA
-    super().__init__(f_shape, **kwargs)
+    super().__init__(address,source_index,f_shape, **kwargs)
 
   def prepare_var_args(self, *args, **kwargs):
-    self.shape = args[0]
+    if len(args) > 2:
+      self.shape = args[2]
+    else:
+      self.shape = ()
     return super().prepare_var_args(*args, **kwargs)
 
   def set_weighted_defaults(self):
@@ -28,9 +31,22 @@ class CollectorSegment(PlasticComp):
     super().set_weighted_defaults(shape=shape,weights=ones(shape))
 
   @property
-  def fill_shape(self):
+  def address(self):
     return self.var[0]
-  
+  @address.setter
+  def address(self, value):
+    self.setter_error()
+    
+  @property
+  def source_index(self):
+    return self.var[1]
+  @source_index.setter
+  def source_index(self, value):
+    self.setter_error()
+
+  @property
+  def fill_shape(self):
+    return self.var[2]
   @fill_shape.setter
   def fill_shape(self, value):
     self.setter_error()
--- tests/components/data/test_collector_segment.py
+++ tests/components/data/test_collector_segment.py
@@ -4,21 +4,25 @@ from hypothesis import given
 from hypothesis import strategies as st
 from numpy import array, array_equal, ones
 
+from components.vars.data import Address
 from components.data.collector_segment import CollectorSegment
 from components.enums.pos import CtgType
 from tests.components.base.test_plastic_comp import TestPlasticComp
 from tests.strategies.data_strats import valid_resource_data, valid_shape, valid_weights
 from utils.pos import Pos
+from tests.strategies.pos_strats import arb_addr
 
 class TestCollectorSegment(TestPlasticComp):
   def set_up_base(self):
     self.ctg = CtgType.DATA
-    self.label = 'TotallyValidId'
+    self.label = 'dend_abov_a_segment_2'
     
   def set_up_var(self):
+    self.address = Address(golem='a',matrix='l',func_set='glg', stage='prim', group='assoc_from', packager='star_0', instruction='dend_above_a')
+    self.source_index = (0,0)
     self.fill_shape = (4,4)
     self.weights = ones(self.fill_shape)
-    self.values = [self.fill_shape]
+    self.values = [self.address, self.source_index, self.fill_shape]
     self.var = tuple(self.values)
 
   def set_up_defaults(self):
@@ -35,38 +39,51 @@ class TestCollectorSegment(TestPlasticComp):
     self.set_up_defaults()
     self.set_up_var()
     self.set_up_dynamic_props()
-    self.comp = CollectorSegment(self.fill_shape,label=self.label)
+    self.comp = CollectorSegment(self.address,self.source_index,self.fill_shape,label=self.label)
 
-  @given(valid_shape()) # pylint: disable=no-value-for-parameter
-  def test_prepare_var_args(self, f_shape):
-    var_args = [f_shape]
-    expectation = tuple([f_shape])
+  @given(arb_addr(), valid_shape(), valid_shape()) # pylint: disable=no-value-for-parameter
+  def test_prepare_var_args(self, address, source_index, f_shape):
+    var_args = [address, source_index, f_shape]
+    expectation = tuple(var_args)
     result = self.comp.prepare_var_args(*var_args)
     self.assertEqual(result, expectation)
-    self.assertEqual(result[0], f_shape)
-    self.assertEqual(self.comp.shape, result[0])
+    self.assertEqual(result[2], f_shape)
+    self.assertEqual(self.comp.shape, result[2])
 
   def test_get_var(self):
     for i in range(len(self.comp.var)):
       self.assertTrue(array_equal(self.comp.var[i], self.var[i]))
 
+  def test_get_address(self):
+    self.assertEqual(self.comp.address, self.address)
+  def test_set_address(self):
+    with self.assertRaises(RuntimeError):
+      self.comp.address = 'Does not matter'
+
+  def test_get_source_index(self):
+    self.assertEqual(self.comp.source_index, self.source_index)
+  def test_set_source_index(self):
+    with self.assertRaises(RuntimeError):
+      self.comp.source_index = 'Does not matter'
+
   def test_get_fill_shape(self):
     self.assertEqual(self.comp.fill_shape, self.fill_shape)
-
   def test_set_fill_shape(self):
     with self.assertRaises(RuntimeError):
       self.comp.fill_shape = 'Does not matter'
 
-  @given(valid_shape()) # pylint: disable=no-value-for-parameter
-  def test_update(self,f_shape):
-    self.comp.update(f_shape)
-    self.assertEqual(self.comp.var, tuple([f_shape]))
+  @given(arb_addr(), valid_shape(), valid_shape()) # pylint: disable=no-value-for-parameter
+  def test_update(self,address, source_index, f_shape):
+    self.comp.update(address, source_index,f_shape)
+    self.assertEqual(self.comp.var, tuple([address,source_index,f_shape]))
     self.assertEqual(self.comp.weights.shape, f_shape)
 
-  def test_reset_with_baseline(self):
+  @given(arb_addr(), valid_shape(), valid_shape()) # pylint: disable=no-value-for-parameter
+  def test_reset_with_baseline(self,address, source_index, f_shape):
+    self.comp = CollectorSegment(self.address,self.source_index,self.fill_shape,label=self.label)
     self.comp.baseline = self.baseline
-    self.comp.update((8,8))
-    self.assertTrue(array_equal(self.comp.fill_shape, (8,8)))
+    self.comp.update(address,source_index,f_shape)
+    self.assertTrue(array_equal(self.comp.fill_shape, f_shape))
     self.comp.reset()
     self.assertEqual(self.comp.var,tuple(self.baseline))
 
