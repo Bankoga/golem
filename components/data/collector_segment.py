61fd992bde31721c50cb892def8ea5f0cee69014
v <bankoga@gmail.com>
2019-03-28 10:53:16 -0500

    updated shape and fill shape connection

--- components/data/collector_segment.py
+++ components/data/collector_segment.py
@@ -12,11 +12,21 @@ class CollectorSegment(PlasticComp, Segment):
       kwargs['label'] = ''
     kwargs['ctg']=CtgType.DATA
     super().__init__(*args, **kwargs)
-    self.shape = self.fill_shape
+    self.__fill_shape = kwargs['fill_shape']
+    self.shape = kwargs['fill_shape']
 
   def set_weighted_defaults(self):
     shape = tuple([1])
     super().set_weighted_defaults(shape=shape,weights=ones(shape))
+
+  @property
+  def fill_shape(self):
+    return self.__fill_shape
+  
+  @fill_shape.setter
+  def fill_shape(self, value):
+    self.__fill_shape = value
+    self.shape = value
 
   def update(self, *args):
     super().update(*args)
