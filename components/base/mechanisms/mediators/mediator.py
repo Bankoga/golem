c0eca35a210ffd98750f0be72e446040efd1c728
v <bankoga@gmail.com>
2019-03-29 10:20:51 -0500

    added comment to mediator

--- components/base/mechanisms/mediators/mediator.py
+++ components/base/mechanisms/mediators/mediator.py
@@ -29,3 +29,23 @@ class Mediator(Mechanism):
   #   so that we can get_card_index(ind, inputs)
   #   context.get_inputs_at_steps_in(direction, inputs)
   #   input_pack = select the inputs using our Pos info plus the card index
+  
+  # @given(processed_module_input_set()) # pylint: disable=no-value-for-parameter
+  # def test_operate(self, inputs):
+  #   """
+  #   At a cell level, we execute all of our instructions using a method provided by the context
+  #   At an instruction level, we execute on the inputs within context in the direction specified using the contextual cardinator
+  #   for each direction
+  #     take a sample of the package shape for each shape
+  #     record activity information while sampling
+  #   combine all samples into a single result using distance attenuation
+  #   """
+  #   packages = inputs[0]
+  #   fs = inputs[1]
+  #   result = self.comp.operate(packages,fs)
+  #   parts = []
+  #   # for pack in inputs:
+  #   # for each instruction
+  #   #   we grab specifed_input from the inputs
+  #   #   we apply a 2d convolution with the specified shape
+  #   #   we do what with the result?
