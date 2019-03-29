6de1d6952379acb14a3786c5ac2e96dfb98f74d4
v <bankoga@gmail.com>
2019-03-29 10:18:59 -0500

    moved comments for behavior to mediator

--- components/base/mechanisms/mediators/mediator.py
+++ components/base/mechanisms/mediators/mediator.py
@@ -19,3 +19,13 @@ class Mediator(Mechanism):
   @channel_registry.setter
   def channel_registry(self, value):
     raise RuntimeError('Cannot set channel registry!')
+  
+  # def collect_relative_addresses():
+  # for each read, we change the Z in the direction supplied using the PROPER cardinator
+  #  we also change the size of the sample
+  #   this means we either have to be given the cardinator
+  #   OR
+  #   we have to be able to select the cardinator
+  #   so that we can get_card_index(ind, inputs)
+  #   context.get_inputs_at_steps_in(direction, inputs)
+  #   input_pack = select the inputs using our Pos info plus the card index
--- components/instructions/collector.py
+++ components/instructions/collector.py
@@ -118,14 +118,6 @@ class Collector(Instruction):
   # for nested cardinal rotations, apply each rotation by its value/the number of rotations
   def instruction_details(self,curr_data=[],inputs=None,context=None):
     res = 0
-    # for each read, we change the Z in the direction supplied using the PROPER cardinator
-    #  we also change the size of the sample
-    #   this means we either have to be given the cardinator
-    #   OR
-    #   we have to be able to select the cardinator
-    #   so that we can get_card_index(ind, inputs)
-    #   context.get_inputs_at_steps_in(direction, inputs)
-    #   input_pack = select the inputs using our Pos info plus the card index
     #   patch = extract a slice from the input_pack according to the current shape
     #   step_res = shape.weights * patch
     #   res.append(step_res * abs(diff(ind,self.pos.z)))
