418ca4b53035b0814e7aca4c8825d6ac57e730b0
v <bankoga@gmail.com>
2019-04-02 11:01:18 -0500

    split roll, draw, and draw from into chaos moved out of prop gen help

--- tests/utils/helpers/test_prop_gen_help.py
+++ tests/utils/helpers/test_prop_gen_help.py
@@ -5,26 +5,11 @@ from hypothesis import strategies as st
 
 from components.axioms.props import pg_data
 from tests.strategies.prop_strats import arb_label
-from utils.helpers.prop_gen_help import draw, draw_from, roll, roll_for_syllables, produce_syllables, roll_name, kin_label_gen_unique, roll_new_name
+from utils.helpers.prop_gen_help import roll_for_syllables, produce_syllables, roll_name, kin_label_gen_unique, roll_new_name
 from utils.validators.prop_validators import is_valid_label
 import re
 
 class TestPropGenHelp(unittest.TestCase):
-
-  @given(st.integers(min_value=2, max_value=30))
-  def test_draw(self, num_sides):
-    res = draw(num_sides)
-    self.assertTrue(0 <= res and res < num_sides)
-
-  @given(st.lists(st.integers(min_value=2, max_value=30),min_size=2, max_size=30))
-  def test_draw_from(self, drawable):
-    res = draw_from(drawable)
-    self.assertTrue(res)
-
-  @given(st.integers(min_value=2, max_value=30))
-  def test_roll(self, num_sides):
-    res = roll(num_sides)
-    self.assertTrue(0 < res and res <= num_sides)
 
   @given(st.integers(min_value=1, max_value=30))
   def test_produce_syllables(self,num_syllables):
