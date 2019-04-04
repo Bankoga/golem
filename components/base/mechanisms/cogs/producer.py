8fb3401d644b1a6c912617c3a3ab6684a2ff306d
v <bankoga@gmail.com>
2019-04-04 09:24:20 -0500

    deleted packager it was replaced functionally by the producer component type

--- components/base/mechanisms/cogs/producer.py
+++ components/base/mechanisms/cogs/producer.py
@@ -1,5 +1,27 @@
 from components.base.mechanisms.mechanism import Mechanism
 
 class Producer(Mechanism):
+  """
+  The actual algorithms that power the matrix
+  Not the function sets, or the meta-containers, or regions, but a unified algorithm or method
+  Examples of producer Types:
+    - Cell
+    - Framework Function
+  We are going to need another metaprovider here.
+  All packagers use multiple instructions to transform input into output
+  """
+  # def __init__(self, rule_type, arb_label):
+  #   super().__init__(arb_label, rule_type.get_component_type(),rule_type)
+  #   self.read_data()
+  # @abstractmethod
+  # def pack(self, inputs):
+  #   pass
+  # @abstractmethod
+  # def read_data(self):
+  #   self.freq_range = prd['freq_range']
+  #   self.init_freq = prd['init_freq']
+  #   self.pct_of_pod = prd['pct_of_pod']
+  #   self.init_threshhold = prd['init_threshhold']
+  #   self.activation_function = prd['activation_function']
   def __init__(self, **kwargs):
     super().__init__(**kwargs)
--- tests/components/base/mechanisms/cogs/test_producer.py
+++ tests/components/base/mechanisms/cogs/test_producer.py
@@ -30,6 +30,17 @@ class TestProducer(TestMechanism):
     self.set_up_var()
     self.comp = Producer(label=self.label, ctg=self.ctg)
     self.comp.build(*self.values)
+    
+  # @given(node_type_prop(),st.from_regex(old_label_pattern)) # pylint: disable=no-value-for-parameter
+  # def test_base_rule(self, arb_type, arb_label):
+  #   rule = Packager(arb_type, arb_label)
+  #   self.assertEqual(rule.ctg_type, arb_type)
+  #   self.assertEqual(rule.get_id(), arb_label)
+  #   self.assertEqual(self.rule.freq_range, prd['freq_range'])
+  #   self.assertEqual(self.rule.init_freq, prd['init_freq'])
+  #   self.assertEqual(self.rule.pct_of_pod, prd['pct_of_pod'])
+  #   self.assertEqual(self.rule.init_threshhold, prd['init_threshhold'])
+  #   self.assertEqual(self.rule.activation_function, prd['activation_function'])
 
 if __name__ == '__main__':
   unittest.main()
