cea417ba3b15a54539d3878be3ac9727c21bc82d
v <bankoga@gmail.com>
2019-04-11 16:44:12 -0500

    added group type to group implementation

--- components/mediators/group.py
+++ components/mediators/group.py
@@ -1,5 +1,15 @@
 from components.base.mechanisms.mechanism import Mechanism
+from components.enums.pos import CtgType
 
 class Group(Mechanism):
   def __init__(self, *args, **kwargs):
+    kwargs['ctg'] = CtgType.GROUP
     super().__init__(*args,**kwargs)
+
+  @property
+  def group_type(self):
+    return self.var[1]
+  
+  @group_type.setter
+  def group_type(self,value):
+    self.setter_error()
--- tests/components/mediators/test_group.py
+++ tests/components/mediators/test_group.py
@@ -3,9 +3,45 @@ import unittest
 from hypothesis import given
 from hypothesis import strategies as st
 
-class TestGroup(unittest.TestCase):
+from components.mediators.group import Group
+from components.enums.prop_types import GroupType
+
+from components.enums.module import ModuleType
+from components.enums.pos import CtgType
+from components.matrix.lineage_registry import LineageRegistry
+from components.matrix.channel_registry import ChannelRegistry
+from components.vars.data import Lineage
+from tests.components.base.mechanisms.test_mechanism import TestMechanism
+
+class TestGroup(TestMechanism):
+  def set_up_base(self):
+    self.label = 'noise_dwn_inhib'
+    self.ctg = CtgType.GROUP
+    self.comp_class = Group
+
+  def set_up_var(self):
+    self.registry = LineageRegistry(label='global_lineage_registry_api')
+    self.lineage_registry = self.registry
+    self.lineage = Lineage(golem='arith_brain',matrix='left',module='thalamus', stage='noise_ctrl',group=self.label)
+    self.reg_item = {
+      'reg_id': self.label,
+      'lineage': self.lineage
+    }
+    self.group_type = GroupType.ORGANO
+    self.values = [self.registry, self.group_type]
+    self.var = tuple(self.values)
+    self.baseline = self.values
+
   def setUp(self):
-    pass
+    self.set_up_base()
+    self.set_up_var()
+    self.comp = self.comp_class(*self.values,label=self.label)
+
+  def test_get_group_type(self):
+    self.assertEqual(self.comp.group_type, self.group_type)
+  def test_set_group_type(self):
+    with self.assertRaises(RuntimeError):
+      self.comp.group_type = self.group_type
 
 if __name__ == '__main__':
   unittest.main()
