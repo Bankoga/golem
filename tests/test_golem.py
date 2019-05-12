9ae0798de841fade16304146262818b5816ce00c
v <bankoga@gmail.com>
2019-05-12 16:49:10 -0500

    rename core to old core added new golem main file

--- tests/test_golem.py
+++ tests/test_golem.py
@@ -1,17 +1,17 @@
-# import unittest
-# from yaml import load, dump
-# import unittest
-# from hypothesis import given
-# import hypothesis.strategies as st
-# 
-# from components.axioms.props import dest_key_pattern 
-# from utils.config_reader import read
-# from golem.core import Golem
+import unittest
+from yaml import load, dump
+import unittest
+from hypothesis import given
+import hypothesis.strategies as st
 
-# class TestGolem(unittest.TestCase):
+from components.axioms.props import dest_key_pattern 
+from utils.config_reader import read
+from golem import Golem
 
-#   def setUp(self):
-#     self.golem = Golem()
+class TestGolem(unittest.TestCase):
 
-# if __name__ == '__main__':
-#     unittest.main()
+  def setUp(self):
+    self.golem = Golem()
+
+if __name__ == '__main__':
+    unittest.main()
